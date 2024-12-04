928d491f5ae830efbb3924c35f542dad92e4166e
nick <nickkarpo@gmail.com>
2024-12-03 23:49:50 -0500

    searchbar css

--- weather-dashboard/src/components/SearchBar/SearchBar.jsx
+++ weather-dashboard/src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import styles from './SearchBar.module.css';
 
 const SearchBar = () => {
 
     const [city, setCity] = useState("");
-    const [results, setResults] = useState(null);
+    const [results, setResults] = useState([]);
 
     const handleInput = async (event) => {
         setCity(event.target.value);
@@ -19,29 +20,32 @@ const SearchBar = () => {
         )
             .then((response) => {
                 if (!response.ok) throw new Error("Error fetching data");
+                console.log(city);
                 return response.json();
             })
             .then((data) => {
-                setResults(data);
-                console.log(data);
+                if (data.cod) throw new Error("No geodata");
+                else {
+                    setResults(data);
+                    console.log(data);
+                }
             })
             .catch((error) => {
                 console.error(error.message);
             });
     };
-
     return (
         <div>
             <h2>Cities</h2>
             <form className="searchCities">
-                <input type="text" name="city" id="city" onChange={handleInput} />
-                <button onClick={fetchCities}>Search</button>
+                <input type="text" name="city" id="city" onChange={handleInput} onKeyUp={fetchCities} />
+
             </form>
             {results && (
                 <div>
                     {results.map((results, index) => (
                         <div key={index}>
-                            <p>{results.name}, {results.state}, {results.country}</p>
+                            <div id={"city" + index} className={styles.city} onClick={checkDetails(index)}>{results.name}, {results.state}, {results.country}</div>
                         </div>
                     ))}
                 </div>
@@ -49,5 +53,7 @@ const SearchBar = () => {
         </div>
     );
 };
-
+function checkDetails(id) {
+    // console.log(id);
+}
 export default SearchBar;
